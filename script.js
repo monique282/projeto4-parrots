e2683d1e732e81adb842a88128c6164ef08ae7f2
monique <moniquejp16@gmail.com>
2023-04-08 12:57:18 -0300

    cartas aparecendo com o gif

--- script.js
+++ script.js
@@ -2,25 +2,51 @@
 let nCartas = 0;
 
 
-while (nCartas < 4 || nCartas > 14 || (nCartas%2 !== 0) )  {
-  nCartas = prompt(' REGRA DO JOGO\n- Número máximo de cartas, 14\n - Número minimo de cartas ,4 \n-Escolha o numero de cartas');
+while (nCartas < 4 || nCartas > 14 || (nCartas % 2 !== 0)) {
+    nCartas = prompt(' REGRA DO JOGO\n- Número máximo de cartas, 14\n - Número minimo de cartas ,4 \n-Escolha o numero de cartas');
 
 }
 
-const cartas =[nCartas];
-console.log(cartas);
 
-const caixaDeCartas = document.querySelector('.caixa-de-cartas');
+let nPares = nCartas / 2;
+
+const imagens = [
+    "bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "unicornparrot.gif"];
+
+let listaBase = [];
+
+for (let n = 0; n < nCartas; n++) {
+    listaBase[n] = ` <div class="carta" onclick ="virarCarta(this)">
+                            <div class="front-face face-1">
+                              <img src="./imagens/${imagens[n]}"/>
+                            </div>
+                            <div class="back-face face ">
+                            <img src="./imagens/front 1.png" alt="">
+                            </div>
+                        </div>
+                        `;
+
+}
+
+let listaDisplay = [];
+for (let i = 0; i < nPares; i++) {
+    listaDisplay.push(listaBase[i]);
+    listaDisplay.push(listaBase[i]);
+}
+
+let caixaDeCartas = document.querySelector('.caixa-de-cartas');
 const cartaInicial = document.querySelector('.carta');
-cartaInicial.remove();
+caixaDeCartas.innerHTML = '&nbsp;';
 
 
-for(let n = 0; n < nCartas ; n++){
-const qtdCartas = document.createElement('div');
-qtdCartas.className = 'carta';
-qtdCartas.innerHTML = '<img src="./imagens/front 1.png" alt="">';
-caixaDeCartas.appendChild(qtdCartas);
+// embaralhar as cartas
+listaDisplay.sort(function() { return 0.5 - Math.random() });
+
+// distribuir as cartas na tela
+for(let m = 0 ; m < nCartas; m++) {
+    caixaDeCartas.innerHTML += listaDisplay[m];
+   
 }
+function virarCarta (){
 
-
-
+}
